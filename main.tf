fe3b993b2f81b1f28dfc06af4863675864e287cf
praveen-fgt <45425712+praveen-fgt@users.noreply.github.com>
2021-08-15 12:35:57 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,18 @@
+provider "google" {
+project = "project-322304"
+region = "us-west1"
+}
+
+resource "google_compute_instance" "myvm" {
+name = "myvm-dev"
+zone = "us-west1-c"
+boot_disk {
+initialize_params {
+  image = "debian-cloud/debian-9"
+  size = "10"
+}
+}
+machine_type = "f1-micro" 
+network_interface {
+network = "default"
+}
