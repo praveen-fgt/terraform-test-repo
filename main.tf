b64ab59acb51b76c3800d0b41a4c32a1fa4cce83
praveen-fgt <45425712+praveen-fgt@users.noreply.github.com>
2021-08-15 13:00:11 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,11 @@
 provider "google" {
 project = "project-322304"
-region = "us-west2"
+region = "asia-south2"
 }
 
 resource "google_compute_instance" "myvm" {
 name = "myvm-dev"
-zone = "us-west1-c"
+zone = "asia-south2-c"
 boot_disk {
 initialize_params {
   image = "debian-cloud/debian-9"
